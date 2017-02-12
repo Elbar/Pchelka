894773925ba663f9a3bd23edacfd3bfe14c71df5
Vista <vista-e@mail.ru>
2017-02-12 14:35:53 +0300

    ^ Edited SessionManager class

---
+++ app/src/main/java/com/kg/vista/beeservice/manager/SessionManager.java
@@ -0,0 +1,107 @@
+package com.kg.vista.beeservice.manager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import com.kg.vista.beeservice.activity.LoginActivity;
+
+import java.util.HashMap;
+
+public class SessionManager {
+    SharedPreferences pref;
+
+    Editor editor;
+
+    Context _context;
+
+    int PRIVATE_MODE = 0;
+
+    private static final String PREF_NAME = "BeeServiceClientPref";
+    private static final String IS_LOGIN = "IsLoggedIn";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_VERIFICATION_CODE = "verification_code";
+
+    public SessionManager(Context context){
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    /**
+     * Create login session
+     * */
+    public void createLoginSession(String name, String verification_code){
+        // Storing login value as TRUE
+        editor.putBoolean(IS_LOGIN, true);
+
+        // Storing name in pref
+        editor.putString(KEY_NAME, name);
+
+        // Storing email in pref
+        editor.putString(KEY_VERIFICATION_CODE, verification_code);
+
+        // commit changes
+        editor.commit();
+    }
+
+    /**
+     * Check login method wil check user login status
+     * If false it will redirect user to login page
+     * Else won't do anything
+     * */
+    public void checkLogin(){
+        // Check login status
+        if(!this.isLoggedIn()){
+            // user is not logged in redirect him to Login Activity
+            Intent i = new Intent(_context, LoginActivity.class);
+            // Closing all the Activities
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            // Add new Flag to start new Activity
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            // Starting Login Activity
+            _context.startActivity(i);
+        }
+
+    }
+
+    /**
+     * Get stored session data
+     * */
+    public HashMap<String, String> getUserDetails(){
+        HashMap<String, String> user = new HashMap<String, String>();
+
+        user.put(KEY_NAME, pref.getString(KEY_NAME, null));
+
+        user.put(KEY_VERIFICATION_CODE, pref.getString(KEY_VERIFICATION_CODE, null));
+
+        return user;
+    }
+
+    /**
+     * Clear session details
+     * */
+    public void logoutUser(){
+        editor.clear();
+        editor.commit();
+
+        Intent i = new Intent(_context, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        // Starting Login Activity
+        _context.startActivity(i);
+    }
+
+    /**
+     * Quick check for login
+     * **/
+    // Get Login State
+    public boolean isLoggedIn(){
+        return pref.getBoolean(IS_LOGIN, false);
+    }
+}
