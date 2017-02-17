e22fa4efd9d9fd0e6b5907e005158071e26813b3
Vista <vista-e@mail.ru>
2017-02-17 11:53:28 +0300

    + Added Theme For ProgressDialog

--- app/src/main/java/com/kg/vista/beeservice/activity/ProfileActivity.java
+++
@@ -1,8 +0,0 @@
-package com.kg.vista.beeservice.activity;
-
-/**
- * Created by Vista on 14.02.2017.
- */
-
-public class ProfileActivity {
-}
---
+++ app/src/main/java/com/kg/vista/beeservice/fragment/MyProfileFragment.java
@@ -0,0 +1,8 @@
+package com.kg.vista.beeservice.fragment;
+
+/**
+ * Created by Vista on 14.02.2017.
+ */
+
+public class MyProfileFragment {
+}
