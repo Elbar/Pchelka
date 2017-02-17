556313b5397d0e111021a6b5b8e30fe4844b9406
Vista <vista-e@mail.ru>
2017-02-17 11:58:57 +0300

    + Added MyProfileFragment|fragment_my_profile & * Modified SessionManager|activity_middleware

--- app/src/main/java/com/kg/vista/beeservice/fragment/MyProfileFragment.java
+++ app/src/main/java/com/kg/vista/beeservice/fragment/MyProfileFragment.java
@@ -1,8 +1,90 @@
 package com.kg.vista.beeservice.fragment;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.kg.vista.beeservice.R;
+import com.kg.vista.beeservice.activity.DrawerActivity;
+import com.kg.vista.beeservice.activity.LoginActivity;
+import com.kg.vista.beeservice.activity.MiddleWareActivity;
+import com.kg.vista.beeservice.manager.SessionManager;
+
+import java.util.HashMap;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * Created by Vista on 14.02.2017.
  */
 
-public class MyProfileFragment {
+public class MyProfileFragment extends Fragment {
+
+    @BindView(R.id.my_profile_name)
+    TextView mProfileName;
+    @BindView(R.id.my_profile_pass)
+    TextView mProfilePass;
+
+    @BindView(R.id.my_profile_logout) Button mProfileLogout;
+    private Unbinder unbinder;
+    SessionManager session;
+
+
+    public MyProfileFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_my_profile, container, false);
+
+
+        unbinder = ButterKnife.bind(this, view);
+
+        session = new SessionManager(getActivity());
+
+        HashMap<String, String> user = session.getUserDetails();
+        String name = user.get(SessionManager.KEY_NAME);
+        String verification_code = user.get(SessionManager.KEY_VERIFICATION_CODE);
+
+        mProfileName.setText(name);
+
+        mProfilePass.setText(verification_code);
+
+        mProfileLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                session.logoutUser();
+                Intent i = new Intent(getActivity(), LoginActivity.class);
+                startActivity(i);
+
+
+
+            }
+        });
+
+
+        return view;
+    }
 }
--- app/src/main/java/com/kg/vista/beeservice/manager/SessionManager.java
+++ app/src/main/java/com/kg/vista/beeservice/manager/SessionManager.java
@@ -33,16 +33,12 @@ public class SessionManager {
      * Create login session
      * */
     public void createLoginSession(String name, String verification_code){
-        // Storing login value as TRUE
         editor.putBoolean(IS_LOGIN, true);
 
-        // Storing name in pref
         editor.putString(KEY_NAME, name);
 
-        // Storing email in pref
         editor.putString(KEY_VERIFICATION_CODE, verification_code);
 
-        // commit changes
         editor.commit();
     }
 
@@ -52,17 +48,12 @@ public class SessionManager {
      * Else won't do anything
      * */
     public void checkLogin(){
-        // Check login status
         if(!this.isLoggedIn()){
-            // user is not logged in redirect him to Login Activity
             Intent i = new Intent(_context, LoginActivity.class);
-            // Closing all the Activities
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-            // Add new Flag to start new Activity
             i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-            // Starting Login Activity
             _context.startActivity(i);
         }
 
@@ -93,7 +84,6 @@ public class SessionManager {
 
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-        // Starting Login Activity
         _context.startActivity(i);
     }
 
