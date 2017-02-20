c57dc09d976f5e1cacefc0912f666c7c2e79eab3
Vista <vista-e@mail.ru>
2017-02-20 19:56:52 +0300

    + Added new ids|strings

---
+++ app/src/main/java/com/kg/vista/beeservice/activity/ChooseSubCategory.java
@@ -0,0 +1,8 @@
+package com.kg.vista.beeservice.activity;
+
+/**
+ * Created by Vista on 18.02.2017.
+ */
+
+public class ChooseSubCategory {
+}
---
+++ app/src/main/java/com/kg/vista/beeservice/activity/SettingsActivity.java
@@ -0,0 +1,8 @@
+package com.kg.vista.beeservice.activity;
+
+/**
+ * Created by Vista on 20.02.2017.
+ */
+
+public class SettingsActivity {
+}
