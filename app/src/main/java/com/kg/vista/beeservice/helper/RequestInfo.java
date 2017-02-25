3efd782d00d2583567958467493da94169e6ca13
Vista <vista-e@mail.ru>
2017-02-25 06:08:59 +0300

    +Added up_rate_button style|card_layout and etc.

--- app/src/main/java/com/kg/vista/beeservice/helper/RequestInfo.java
+++
@@ -1,8 +0,0 @@
-package com.kg.vista.beeservice.helper;
-
-/**
- * Created by Vista on 22.02.2017.
- */
-
-public class RequestInfo {
-}
---
+++ app/src/main/java/com/kg/vista/beeservice/watcher/PhoneNumberFormattingTextWatcher.java
@@ -0,0 +1,8 @@
+package com.kg.vista.beeservice.watcher;
+
+/**
+ * Created by Vista on 21.02.2017.
+ */
+
+public class PhoneNumberFormattingTextWatcher {
+}
