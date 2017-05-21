cd9bd06a9c7227cff312ab1bd139b0ef7380c3ac
Elbar <vista-e@mail.ru>
2017-05-21 16:32:02 +0600

    +Added ormlite to build.gradle|Added db models and db dao

---
+++ app/src/main/java/com/kg/vista/beeserviceclient/db/dao/MyRequestDetailsDao.java
@@ -0,0 +1,8 @@
+package com.kg.vista.beeserviceclient.db.dao;
+
+/**
+ * Created by fen on 21.05.2017.
+ */
+
+public class MyRequestDetailsDao {
+}
---
+++ app/src/main/java/com/kg/vista/beeserviceclient/db/model/NewRequestDetails.java
@@ -0,0 +1,8 @@
+package com.kg.vista.beeserviceclient.db.model;
+
+/**
+ * Created by fen on 21.05.2017.
+ */
+
+public class NewRequestDetails {
+}
