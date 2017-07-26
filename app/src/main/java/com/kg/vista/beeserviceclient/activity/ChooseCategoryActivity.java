70620aca5bba8cc866f65b9f89e8fc9b39c8c7f0
Elbar <Elbar_israilov@mail.ru>
2017-07-27 01:46:22 +0600

    +Added logo to taxi|and set debu to false

--- app/src/main/java/com/kg/vista/beeserviceclient/activity/ChooseCategoryActivity.java
+++ app/src/main/java/com/kg/vista/beeserviceclient/activity/ChooseCategoryActivity.java
@@ -1,8 +1,6 @@
 package com.kg.vista.beeserviceclient.activity;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
@@ -10,31 +8,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ListView;
 
 import com.kg.vista.beeserviceclient.R;
 import com.kg.vista.beeserviceclient.adapter.CustomListAdapter;
-import com.kg.vista.beeserviceclient.manager.AlertDialogManager;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class ChooseCategoryActivity extends AbstractActivity {
-
-    final AlertDialogManager alert = new AlertDialogManager();
     @BindView(R.id.category_lv)
     ListView mCategoryListView;
     Integer[] imageArray = {
@@ -131,117 +114,6 @@ public class ChooseCategoryActivity extends AbstractActivity {
         if (ab != null) {
             ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
             ab.setCustomView(R.layout.category_actionbar);
-        }
-    }
-
-    public class CategoryTask extends AsyncTask<Void, Void, String> implements OnItemSelectedListener {
-
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
-        String resultJson = "";
-        ProgressDialog pdLoading = new ProgressDialog(ChooseCategoryActivity.this);
-
-
-        @Override
-        protected String doInBackground(Void... params) {
-
-
-            try {
-
-                URL url = new URL("http://176.126.167.34/api/v1/category/");
-
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuilder buffer = new StringBuilder();
-
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line);
-                }
-
-                resultJson = buffer.toString();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return resultJson;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pdLoading.setMessage("Загрузка...");
-            pdLoading.setIndeterminate(true);
-            pdLoading.setCancelable(false);
-            pdLoading.show();
-
-
-        }
-
-        private void dismissProgressDialog() {
-
-            try {
-
-                if (pdLoading != null && pdLoading.isShowing()) {
-                    pdLoading.dismiss();
-                }
-            } catch (Exception e) {
-
-                e.printStackTrace();
-            } finally {
-                pdLoading = null;
-            }
-
-        }
-
-
-        @Override
-        protected void onPostExecute(String strJson) {
-            super.onPostExecute(strJson);
-
-            dismissProgressDialog();
-
-            JSONObject dataJsonObj;
-
-            try {
-
-                dataJsonObj = new JSONObject(strJson);
-                JSONArray result = dataJsonObj.getJSONArray("objects");
-                List<String> categories = new ArrayList<>();
-
-
-                for (int i = 0; i < result.length(); i++) {
-                    JSONObject json_data = (JSONObject) result.get(i);
-
-
-                    String cat_name = json_data.getString("name");
-
-                    categories.add(cat_name);
-
-                }
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-
-        }
-
-        @Override
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-
         }
     }
 
